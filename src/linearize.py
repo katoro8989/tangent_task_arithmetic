42fb4ee52aa4043b467ab54150c7c60ed8e26dc6
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-08-24 15:04:45 +0900

    Update linearize.py

--- src/linearize.py
+++ src/linearize.py
@@ -4,8 +4,13 @@ import os
 import torch
 import torch.nn as nn
 from functorch import make_functional_with_buffers
-from torch.func import jvp
 import torch.autograd.functional as F
+
+from copy import deepcopy
+from torch.func import functional_call, jvp
+from collections import OrderedDict
+
+
 
 
 
@@ -69,8 +74,8 @@ class LinearizedModel(nn.Module):
         dparams = [p - p0 for p, p0 in zip(self.params, self.params0)]
         out, dp = F.jvp(
             lambda param: self.func0(param, x),
-            (tuple(self.params0),),  # primals
-            (tuple(dparams),)        # tangents
+            tuple(self.params0),  # primals
+            tuple(dparams),        # tangents
         )
         return out + dp
 
@@ -151,3 +156,69 @@ class LinearizedImageEncoder(abc.ABC, nn.Module):
         state_dict.pop("model_name")
         taylorized_encoder.load_state_dict(state_dict)
         return taylorized_encoder
+    
+
+
+def dict_params_to_tuple(dict_params: dict):
+    return tuple(v for k, v in dict_params.items())
+
+
+class LinearizedModelWraper(nn.Module):
+    def __init__(self, model: nn.Module, init_model: nn.Module = None):
+        """
+        Initializes a linearized model.
+
+        Args:
+            model (nn.Module): The underlying PyTorch model to be linearized.
+            init_model (nn.Module): The initial PyTorch model used to compute the linearization parameters (default: None).
+        """
+        super().__init__()
+        self.model = model
+        if init_model is None:
+            init_model = model
+        assert not hasattr(self, "params0")
+        params0 = deepcopy([(k, v.detach()) for k, v in init_model.named_parameters()])
+        self.params0_keys = [k for k, v in params0]
+        self.params0_values = nn.ParameterList([v for k, v in params0])
+        for p in self.params0_values:
+            p.requires_grad_(False)
+
+    def tuple_params_to_dict(self, tuple_params):
+        """
+        Converts a tuple of parameters to a dictionary with keys corresponding to the parameter names.
+
+        Args:
+            tuple_params (Tuple[Tensor, ...]): A tuple of parameters.
+
+        Returns:
+            Dict[str, Tensor]: A dictionary with keys corresponding to the parameter names and values corresponding to the
+            parameter values.
+        """
+        assert len(tuple_params) == len(self.params0_keys)
+        state_dict = {}
+        for k, p in zip(self.params0_keys, tuple_params):
+            state_dict[k] = p
+        return state_dict
+
+    def forward(self, *args, **kwargs):
+        """
+        Computes the linearized model output using a first-order Taylor decomposition.
+
+        Args:
+            *args: Positional arguments to be passed to the model.
+            **kwargs: Keyword arguments to be passed to the model.
+
+        Returns:
+            torch.Tensor: The output of the linearized model, computed using a first-order Taylor decomposition.
+        """
+        params0 = tuple(self.params0_values)
+        params = dict_params_to_tuple(OrderedDict(self.named_parameters()))
+        dparams = tuple(p - p0 for p, p0 in zip(params, params0))
+        out, dp = jvp(
+            lambda *param: functional_call(
+                self.model, self.tuple_params_to_dict(param), args, kwargs
+            ),
+            params0,
+            dparams,
+        )
+        return out + dp
