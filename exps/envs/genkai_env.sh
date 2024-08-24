5713b29b19cf25d286f63dde841be929b3750fe1
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-08-24 16:27:19 +0900

    Update genkai_env.sh

--- exps/envs/genkai_env.sh
+++ exps/envs/genkai_env.sh
@@ -38,22 +38,22 @@ module load nccl/2.20.5
 eval "module list"
 
 
-# CUDA, cuDNN, NCCLのライブラリパスを統一する
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/var/lib/tcpx/lib64:${LD_LIBRARY_PATH}
+# # CUDA, cuDNN, NCCLのライブラリパスを統一する
+# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/var/lib/tcpx/lib64:${LD_LIBRARY_PATH}
 
-# CUDA 11.8 ライブラリパス
-export LD_LIBRARY_PATH=/apps/t4/rhel9/cuda/11.8.0/extras/CUPTI/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/apps/t4/rhel9/cuda/11.8.0/lib64:$LD_LIBRARY_PATH
+# # CUDA 11.8 ライブラリパス
+# export LD_LIBRARY_PATH=/apps/t4/rhel9/cuda/11.8.0/extras/CUPTI/lib64:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/apps/t4/rhel9/cuda/11.8.0/lib64:$LD_LIBRARY_PATH
 
-# cuDNN 8.9.7 ライブラリパス
-export LD_LIBRARY_PATH=/apps/t4/rhel9/free/cudnn/8.9.7/cuda/11/lib64:$LD_LIBRARY_PATH
+# # cuDNN 8.9.7 ライブラリパス
+# export LD_LIBRARY_PATH=/apps/t4/rhel9/free/cudnn/8.9.7/cuda/11/lib64:$LD_LIBRARY_PATH
 
-# NCCL 2.20.5 ライブラリパス
-export LD_LIBRARY_PATH=/apps/t4/rhel9/free/nccl/2.20.5/cuda11.8.0/lib:$LD_LIBRARY_PATH
+# # NCCL 2.20.5 ライブラリパス
+# export LD_LIBRARY_PATH=/apps/t4/rhel9/free/nccl/2.20.5/cuda11.8.0/lib:$LD_LIBRARY_PATH
 
-# 確認
-echo $LD_LIBRARY_PATH
+# # 確認
+# echo $LD_LIBRARY_PATH
 
 
 eval "nvidia-smi"
