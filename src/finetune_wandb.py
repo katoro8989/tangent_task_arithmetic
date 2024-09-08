b06a856b08c6e4463c7be6f5e45a6569c2eac532
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-09-08 23:03:39 +0900

    Update finetune_wandb.py

--- src/finetune_wandb.py
+++ src/finetune_wandb.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
         # "DTD",
         # "EuroSAT",
         # "GTSRB",
-        "MNIST",
+        # "MNIST",
         "RESISC45",
         "SUN397",
         "SVHN",
