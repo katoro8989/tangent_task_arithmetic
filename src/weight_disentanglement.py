47dc82b98c247d5a5094d1c82a31706272daa82e
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-09-11 08:58:20 +0900

    Update weight_disentanglement.py

--- src/weight_disentanglement.py
+++ src/weight_disentanglement.py
@@ -17,13 +17,13 @@ else:
 
 
 eval_datasets = [
-    "Cars",
-    # "DTD",
+    # "Cars",
+    "DTD",
     # "EuroSAT",
     # "GTSRB",
-    "MNIST",
+    # "MNIST",
     # "RESISC45",
-    # "SVHN",
+    "SVHN",
     # "SUN397",
 ]
 
@@ -69,7 +69,7 @@ for pair_dataset in all_combinations:
 if args.finetuning_mode == "standard":
     save_file = f"{args.save}/weight_disentanglement_standard.json"
 elif args.finetuning_mode == "linear":
-    save_file = f"{args.save}/weight_disentanglement_linear_cars_mnist.json"
+    save_file = f"{args.save}/weight_disentanglement_linear_dtd_svhn.json"
 elif args.finetuning_mode == "posthoc":
     save_file = f"{args.save}/weight_disentanglement_posthoc.json"
 with open(save_file, "w") as f:
