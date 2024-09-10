7c2e7016355ebf220dd533c16fef031f7eded847
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-09-10 20:57:58 +0900

    add

--- src/eval.py
+++ src/eval.py
@@ -15,7 +15,7 @@ def eval_single_dataset(image_encoder, dataset_name, args):
     model = ImageClassifier(image_encoder, classification_head)
 
     model.eval()
-    torch.cuda.set_device(0)
+    torch.cuda.set_device(1)
     args.batch_size = 32
 
     dataset = get_dataset(
--- src/weight_disentanglement.py
+++ src/weight_disentanglement.py
@@ -16,32 +16,12 @@ else:
     args.save = f"/mnt/data/checkpoints/{args.model}"
 
 
-print("*" * 100)
-if args.finetuning_mode == "standard":
-    print("Evaluating non-linear FT models.")
-    ft_accuracies_path = os.path.join(args.save, "ft_accuracies.json")
-elif args.finetuning_mode == "linear":
-    print("Evaluating linear FT models.")
-    ft_accuracies_path = os.path.join(args.save, "linear_ft_accuracies.json")
-elif args.finetuning_mode == "posthoc":
-    print("Evaluating post-hoc linearized models.")
-    ft_accuracies_path = os.path.join(args.save, "posthoc_ft_accuracies.json")
-else:
-    raise ValueError(f"Invalid finetuning mode: {args.finetuning_mode}")
-print("*" * 100)
-
-with open(ft_accuracies_path) as f:
-    args.finetuning_accuracies = json.load(f)
-
-with open(os.path.join(args.save, "zeroshot_accuracies.json")) as f:
-    pretrained_accuracies = json.load(f)
-
 eval_datasets = [
     # "Cars",
     # "DTD",
-    # "EuroSAT",
+    "EuroSAT",
     # "GTSRB",
-    "MNIST",
+    # "MNIST",
     # "RESISC45",
     "SVHN",
     # "SUN397",
@@ -59,8 +39,8 @@ for pair_dataset in all_combinations:
             if args.task_to_orth == "Cars":
                 args.task_to_orth = args.task_to_orth + "Val"
             pretrained_checkpoint = f"{args.save}/{dataset}Val/linear_zeroshot.pt"
-            # finetuned_checkpoint = f"{args.save}/{dataset}Val/linear_finetuned.pt"
-            finetuned_checkpoint = f"{args.save}/{dataset}Val/linear_finetuned_orth_to_{args.task_to_orth}.pt"
+            finetuned_checkpoint = f"{args.save}/{dataset}Val/linear_finetuned.pt"
+            # finetuned_checkpoint = f"{args.save}/{dataset}Val/linear_finetuned_orth_to_{args.task_to_orth}.pt"
             task_vectors.append(
                 LinearizedTaskVector(pretrained_checkpoint, finetuned_checkpoint)
             )
@@ -71,11 +51,11 @@ for pair_dataset in all_combinations:
                 NonLinearTaskVector(pretrained_checkpoint, finetuned_checkpoint)
             )
 
-    args.eval_datasets = [dataset for dataset in pair_dataset]
+    args.eval_datasets = [dataset + "Val" for dataset in pair_dataset]
     args.control_dataset = None
 
     # We use the validation set to choose the optimal coefficient.
-    test_metrics = evaluate_weight_disentanglement(
+    val_metrics = evaluate_weight_disentanglement(
         task_vectors[0],
         task_vectors[1],
         pretrained_checkpoint,
@@ -83,14 +63,14 @@ for pair_dataset in all_combinations:
         posthoc_linearization=args.finetuning_mode == "posthoc",
     )
 
-    weight_disentanglement = {"test": test_metrics}
+    weight_disentanglement = {"val": val_metrics}
     final_weight_disentanglement[f"{pair_dataset[0]}-{pair_dataset[1]}"] = weight_disentanglement
 
 if args.finetuning_mode == "standard":
-    save_file = f"{args.save}/weight_disentanglement.json"
+    save_file = f"{args.save}/weight_disentanglement_standard.json"
 elif args.finetuning_mode == "linear":
-    save_file = f"{args.save}/weight_disentanglement.json"
+    save_file = f"{args.save}/weight_disentanglement_linear.json"
 elif args.finetuning_mode == "posthoc":
-    save_file = f"{args.save}/weight_disentanglement.json"
+    save_file = f"{args.save}/weight_disentanglement_posthoc.json"
 with open(save_file, "w") as f:
     json.dump(final_weight_disentanglement, f, indent=4)
