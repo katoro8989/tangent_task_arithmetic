0de8ca66d5bb497c2480a7c9d019425d2fb4fec1
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-09-14 16:41:47 +0900

    Update finetune_proposed.py

--- src/finetune_proposed.py
+++ src/finetune_proposed.py
@@ -249,8 +249,8 @@ def finetune(rank, args, group):
 if __name__ == "__main__":
     train_datasets = [
         # "Cars",
-        "DTD",
-        # "EuroSAT",
+        # "DTD",
+        "EuroSAT",
         # "GTSRB",
         # "MNIST",
         # "RESISC45",
