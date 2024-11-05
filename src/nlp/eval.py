5a6a6ddaa66b83afe0f6be9a7c449917b268e015
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-11-05 12:00:31 +0900

    Update eval.py

--- src/nlp/eval.py
+++ src/nlp/eval.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-import tqdm
+from tqdm import tqdm
 from sklearn.metrics import accuracy_score, matthews_corrcoef, f1_score
 from scipy.stats import pearsonr, spearmanr
 
