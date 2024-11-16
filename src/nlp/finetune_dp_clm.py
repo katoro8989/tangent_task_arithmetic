82fde2cc10e308ae68178f6096751170c5d9c102
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-11-16 17:29:11 +0900

    Update finetune_dp_clm.py

--- src/nlp/finetune_dp_clm.py
+++ src/nlp/finetune_dp_clm.py
@@ -116,7 +116,10 @@ def finetune(rank, args, group):
             data_time = time.time() - start_time
 
             # モデルの出力を取得
-            logits = ddp_model(input_ids=input_ids)
+            if linearized_finetuning:
+                logits = ddp_model(input_ids=input_ids, labels=input_ids).logits
+            else:
+                logits = ddp_model(input_ids=input_ids)
             shift_logits = logits[..., :-1, :].contiguous()
             shift_labels = labels[..., 1:].contiguous()
 
@@ -163,7 +166,7 @@ def finetune(rank, args, group):
                         data_time = time.time() - start_time
 
                         # モデルの出力を取得
-                        if linearized_finetuning == "linear":
+                        if linearized_finetuning:
                             logits = ddp_model(input_ids=input_ids, labels=input_ids).logits
                         else:
                             logits = ddp_model(input_ids=input_ids)
