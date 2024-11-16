3faeb9c0c02e92cf298dc98d174144d5532a9364
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-11-16 14:44:18 +0900

    Update eval_gpt2_wikitext.py

--- src/nlp/eval_gpt2_wikitext.py
+++ src/nlp/eval_gpt2_wikitext.py
@@ -57,7 +57,7 @@ except FileNotFoundError:
 if args.finetuning_mode == "none":
     model = task_vector.apply_to(pretrained_checkpoint, scaling_coef=0.0)
 elif args.finetuning_mode == "standard" or args.finetuning_mode == "linear" or args.finetuning_mode == "ours":
-    model = task_vector.apply_to(pretrained_checkpoint, scaling_coef=-1.0)
+    model = task_vector.apply_to(pretrained_checkpoint, scaling_coef=1.0)
 
 model = model.to(device)
 model.eval()
