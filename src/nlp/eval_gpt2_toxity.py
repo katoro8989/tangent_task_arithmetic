adcd3b9f15b60b887c965cafa76169be1c64c39c
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-11-16 09:17:20 +0900

    Update eval_gpt2_toxity.py

--- src/nlp/eval_gpt2_toxity.py
+++ src/nlp/eval_gpt2_toxity.py
@@ -52,7 +52,6 @@ try:
     )
 except FileNotFoundError:
     print(f"Error: Could not find {finetuned_checkpoint}.")
-    continue
 
 if args.finetuning_mode == "none":
     model = task_vector.apply_to(pretrained_checkpoint, scaling_coef=0.0)
