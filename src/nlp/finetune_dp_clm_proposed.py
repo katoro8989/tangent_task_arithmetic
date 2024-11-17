9c41f5feaf8cdb64b7872fb78b948eec9d3b4016
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-11-17 09:31:22 +0900

    add

--- src/nlp/finetune_dp_clm_proposed.py
+++ src/nlp/finetune_dp_clm_proposed.py
@@ -165,8 +165,9 @@ def finetune(rank, args, group):
             data_time = time.time() - start_time
 
             # モデルの出力を取得
-            out, dp = ddp_model(input_ids=input_ids)
-            logits = out + dp
+            logits = ddp_model(input_ids=input_ids)
+            # out, dp = ddp_model(input_ids=input_ids)
+            # logits = out + dp
             shift_logits = logits[..., :-1, :].contiguous()
             shift_labels = labels[..., 1:].contiguous()
 
@@ -181,9 +182,9 @@ def finetune(rank, args, group):
                     batch_to_orth = next(ddp_train_loader_iter_orth)
 
                 inputs_to_orth = batch_to_orth["input_ids"].to(device)
-                # tau_jacob = ddp_model.module.dp(input_ids=inputs_to_orth)
-                out, dp = ddp_model(input_ids=inputs_to_orth)
-                tau_jacob = dp
+                tau_jacob = ddp_model.module.dp(input_ids=inputs_to_orth)
+                # out, dp = ddp_model(input_ids=inputs_to_orth)
+                # tau_jacob = dp
                 dp_norms = torch.norm(tau_jacob, dim=1)
                 penalty = dp_norms.mean()
             
@@ -231,9 +232,9 @@ def finetune(rank, args, group):
 
                         # モデルの出力を取得
 
-                        # logits = ddp_model(input_ids=input_ids)
-                        out, dp = ddp_model(input_ids=input_ids)
-                        logits = out + dp
+                        logits = ddp_model(input_ids=input_ids)
+                        # out, dp = ddp_model(input_ids=input_ids)
+                        # logits = out + dp
                         shift_logits = logits[..., :-1, :].contiguous()
                         shift_labels = labels[..., 1:].contiguous()
 
