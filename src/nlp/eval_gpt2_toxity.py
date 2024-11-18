4627bd1c352689a1a210f6e35c0ec61f2aa9c1a1
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-11-18 17:19:34 +0900

    add

--- src/nlp/eval_gpt2_toxity.py
+++ src/nlp/eval_gpt2_toxity.py
@@ -57,7 +57,7 @@ except FileNotFoundError:
 if args.finetuning_mode == "none":
     model = task_vector.apply_to(pretrained_checkpoint, scaling_coef=0.0)
 else:
-    model = task_vector.apply_to(pretrained_checkpoint, scaling_coef=-0.1)
+    model = task_vector.apply_to(pretrained_checkpoint, scaling_coef=-1.0)
 
 model = model.to(device)
 
