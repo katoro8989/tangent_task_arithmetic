4088004dcd8c5d1484929d82d675fdcfca1a0c6a
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-11-20 10:30:15 +0900

    add

---
+++ src/nlp/eval_task_addition_ada.py
@@ -0,0 +1,220 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+import time
+import sys
+from tqdm import tqdm
+import torch
+import gc
+from src.task_vectors import T5NonLinearTaskVector
+from src.eval import eval_single_dataset, eval_single_dataset_head, eval_single_dataset_preprocess_head
+from src.args import parse_arguments
+from datasets import load_from_disk
+from torch.utils.data import DataLoader
+from transformers import T5ForConditionalGeneration
+from linearize import SimpleCallableHFModel
+
+
+def create_log_dir(path, filename='log.txt'):
+    import logging
+    if not os.path.exists(path):
+        os.makedirs(path)
+    logger = logging.getLogger(path)
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(path+'/'+filename)
+    fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
+
+exam_datasets = ["cola", "sst2", "mrpc", "rte"]
+model = "google/flan-t5-small"
+args = parse_arguments()
+args.model = model
+args.seed = 42
+args.data_dir = "/mnt2/dataset/glue_split"
+
+
+if args.seed is not None:
+    args.save = f"/mnt2/t5_glue_checkpoints_{args.seed}/{args.model}"
+else:
+    args.save = f"/mnt2/t5_glue_checkpoints/{args.model}"
+
+args.logs_path = args.save
+pretrained_checkpoint = f"{args.save}/cola/zeroshot"
+
+str_time_ = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
+log = create_log_dir(args.logs_path, 'log_{}_Task_wise_AdaMerging.txt'.format(str_time_))
+args.log = log
+
+task_vectors = [T5NonLinearTaskVector(pretrained_checkpoint, f"{args.save}/{dataset_name}/finetuned") for dataset_name in exam_datasets]
+
+def del_attr(obj, names):
+    if len(names) == 1:
+        delattr(obj, names[0])
+    else:
+        del_attr(getattr(obj, names[0]), names[1:])
+
+def set_attr(obj, names, val):
+    if len(names) == 1:
+        setattr(obj, names[0], val)
+    else:
+        set_attr(getattr(obj, names[0]), names[1:], val)
+
+def make_functional(mod):
+    orig_params = tuple(mod.parameters())
+    names = []
+    for name, p in list(mod.named_parameters()):
+        del_attr(mod, name.split("."))
+        names.append(name)
+    return orig_params, names
+
+def load_weights(mod, names, params):
+    for name, p in zip(names, params):
+        set_attr(mod, name.split("."), p)
+   
+
+
+class ModelWrapper(torch.nn.Module):
+    def __init__(self, model, initial_weights=None):
+        super(ModelWrapper, self).__init__()
+        self.model = model
+
+        if hasattr(self.model, 'transformer'):
+            delattr(self.model, 'transformer')
+
+    def forward(self, images):
+        features = self.model(images)
+        return features
+
+from src.heads import get_classification_head
+class AdaMerging(torch.nn.Module):
+    def __init__(self, paramslist, model, names, exam_datasets):
+        super(AdaMerging, self).__init__()
+        self.paramslist = paramslist
+        self.model = model
+        self.names = names
+        self.pretrain_lambdas = torch.ones(1, 1)
+        prior = 0.3
+        rlambdas = torch.ones(1, len(paramslist)-1) * prior  # (1 * tasks)
+        self.lambdas_raw = torch.nn.Parameter(rlambdas)
+
+    def lambdas(self):
+        task_lambdas = torch.clamp(self.lambdas_raw, min=0.0, max=1.0)
+        lambdass = torch.cat((self.pretrain_lambdas, task_lambdas), 1)
+        return lambdass
+
+    def collect_trainable_params(self):
+        return [self.lambdas_raw]
+
+    def get_image_encoder(self):
+        alph = self.lambdas()
+        params = tuple(sum(tuple(pi * lambdasi for pi, lambdasi in zip(p, alph[0].cpu()))) for j, p in enumerate(zip(*self.paramslist)))
+        params = tuple(p.cuda(0) for p in params)
+        load_weights(self.model, self.names, params)
+        return self.model
+
+    def forward(self, inp, dataset_name):
+        alph = self.lambdas()
+        params = tuple(sum(tuple(pi * lambdasi for pi, lambdasi in zip(p, alph[0].cpu()))) for j, p in enumerate(zip(*self.paramslist)))
+
+        params = tuple(p.cuda(0) for p in params)
+
+        load_weights(self.model, self.names, params)
+        feature = self.model(inp)
+        torch.cuda.empty_cache()
+
+        return out
+
+def softmax_entropy(x):
+    return -(x.softmax(1) * x.log_softmax(1)).sum(1)
+
+hf_t5_model = T5ForConditionalGeneration.from_pretrained(pretrained_checkpoint)
+pretrained_model = SimpleCallableHFModel(hf_t5_model)
+pretrained_model_dic = pretrained_model.state_dict()
+
+model = ModelWrapper(pretrained_model, exam_datasets)
+model = model.to(args.device)
+_, names = make_functional(model)
+
+paramslist = []
+paramslist += [tuple(v.detach().requires_grad_().cpu() for _, v in pretrained_model_dic.items())] # pretrain
+paramslist += [tuple(v.detach().requires_grad_().cpu() for _, v in tv.vector.items())  for i, tv in enumerate(task_vectors)] # task vectors
+torch.cuda.empty_cache()
+adamerging_mtl_model = AdaMerging(paramslist, model, names, exam_datasets)
+
+print('init lambda:')
+print(adamerging_mtl_model.lambdas())
+print('collect_trainable_params:')
+print(list(adamerging_mtl_model.collect_trainable_params()))
+
+epochs = 500
+optimizer = torch.optim.Adam(adamerging_mtl_model.collect_trainable_params(), lr=1e-3, betas=(0.9, 0.999), weight_decay=0.)
+accumulation_steps = 8
+
+from src.datasets.registry import get_dataset
+from src.datasets.common import get_dataloader, maybe_dictionarize
+
+dataloaders = {}
+for dataset_name in exam_datasets:
+    #from args.data_dir/dataset_name
+    task_dir = os.path.join(args.data_dir, dataset_name)
+    encoded_dataset = load_from_disk(task_dir)
+    dataloader = DataLoader(encoded_dataset["test"], batch_size=2, collate_fn=collate_fn, shuffle=True)
+    dataloaders[dataset_name] = dataloader
+data_iters = {dataset_name: iter(dataloaders[dataset_name]) for dataset_name in exam_datasets}
+
+def log_memory_usage(stage):
+    print(f"[{stage}] Memory Allocated: {torch.cuda.memory_allocated() / 1e6} MB")
+    print(f"[{stage}] Memory Reserved: {torch.cuda.memory_reserved() / 1e6} MB")
+
+for epoch in tqdm(range(epochs), desc="Training"):
+    log_memory_usage("Start of Epoch")
+    losses = 0.
+    for dataset_name in exam_datasets:
+        log_memory_usage(f"Before {dataset_name} Forward Pass")
+        try:
+            data_iter = data_iters[dataset_name]
+            data = next(data_iter)
+        except StopIteration:
+            dataloader = dataloaders[dataset_name]
+            data_iters[dataset_name] = iter(dataloader)
+            data_iter = data_iters[dataset_name]
+            data = next(data_iter)
+        log_memory_usage(f"After Getting Data")
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+
+        log_memory_usage(f"Before Prediction")
+        outputs = adamerging_mtl_model(input_ids, attention_mask, labels, dataset_name)
+        log_memory_usage(f"After Prediction")
+        loss = softmax_entropy(outputs).mean(0)
+        losses += loss
+
+        log_memory_usage(f"After {dataset_name} Forward Pass")
+
+        # 不要なテンソルの削除
+        del x, y, outputs, loss
+        torch.cuda.empty_cache()
+        gc.collect()
+
+
+    optimizer.zero_grad()
+    losses.backward()
+    optimizer.step()
+
+
+    if ((epoch+1) % 500) == 0:
+        log.info(str(list(adamerging_mtl_model.lambdas().data)))
+
+        Total_ACC = 0.
+        for dataset_name in exam_datasets:
+            image_encoder = adamerging_mtl_model.get_image_encoder()
+            classification_head = adamerging_mtl_model.get_classification_head(dataset_name)
+            metrics = eval_single_dataset_preprocess_head(image_encoder, classification_head, dataset_name, args)
+            Total_ACC += metrics['top1']
+            log.info('Eval: Epoch: ' + str(epoch) + ' dataset: ' + str(dataset_name) + ' ACC: ' + str(metrics['top1']))
+        log.info('Eval: Epoch: ' + str(epoch) + ' Avg ACC:' + str(Total_ACC / len(exam_datasets)) + '\n')
